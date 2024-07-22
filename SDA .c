9e48655e6b75aa174287ad06a241bb54809843b3
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-07-22 20:01:51 +0530

    Add files via upload

---
+++ SDA .c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define MAX 48
+int main()
+{
+	int A[MAX],limit,i;
+	printf("Enter ur limt value:\n");
+	scanf("%d",&limit);
+	printf("Enter array elements\n");
+	for(i=0;i<limit;i++)
+	{
+		printf("A[%d]=",i);
+		scanf("%d",&A[i]);
+	}
+	printf("Array elements are\n");
+	for(i=0;i<limit;i++)
+	{
+		printf("A[%d]=%d\n",i,A[i]);
+	}
+	return 0;
+}
