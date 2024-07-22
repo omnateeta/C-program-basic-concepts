e725d74e219344ce739aa62c3b3bd3aa78d2497b
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-07-22 19:57:02 +0530

    Add files via upload
    this example contain arithmetic  operator

---
+++ CIE marks calculation.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int CIE1,CIE2,totalcie=0,ASSIGN,QUIZ,PEX,LAB;
+	printf("Enter your first CIE marks out of 25\n\a\a\a");
+	scanf("%d",&CIE1);
+	printf("Enter your first CIE marks out of 25\n");
+	scanf("%d",&CIE2);
+	totalcie=CIE1+CIE2;
+	printf("Total CIE is %d\n",totalcie);
+	
+	
+}
+
