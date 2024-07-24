b28dd9d21fed5d91c8b099c02d79d6efd6299bc7
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-07-24 10:16:57 +0530

    Add files via upload
    bubble short - shorting numbers in ascending order.

---
+++ sorting num in decending order1.c
@@ -0,0 +1,29 @@
+// Shorting the in descending order
+#include<stdio.h>
+int main()
+{
+	int a[20],n,temp,i,j;
+	printf("n number\n");
+	scanf("%d",&n);
+	printf("unsorted array\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]<a[j])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	printf("sorted element\n");
+	for(i=0;i<n;i++)
+	printf("%d\t",a[i]);
+	return 0;
+}
