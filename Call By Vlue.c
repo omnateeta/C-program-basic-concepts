5815c5add5404f2c1b1479ec1d3dddf1a445a730
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-07-27 10:15:22 +0530

    Add files via upload

---
+++ Call By Vlue.c
@@ -0,0 +1,21 @@
+//This program is about call by value
+#include<stdio.h>
+void swapping(int x,int y);
+int main()
+{
+	int a=10,b=20;
+	printf("Before swapping\n");
+	printf("a=%d\nb=%d\n",a,b);
+	swapping(a,b);
+	printf("After swapping\n");
+	printf("a=%d\nb=%d\n",a,b);
+	return 0;
+	
+}
+void swapping(int x,int y)
+{
+	int temp=0;
+	temp=x;
+	x=y;
+	y=temp;
+}
