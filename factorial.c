f5b0fa18c421a8548cddd0d881e8ea884ae6935e
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2024-08-30 08:28:08 -0700

    Add files via upload
    This program is about Factorial of numbers

---
+++ factorial.c
@@ -0,0 +1,16 @@
+//This program is about factorial of numbers
+#include<stdio.h>
+int main()
+{
+	int i,num,fact=1;
+	printf("Enter your factorial number:");
+	scanf("%d",&num);
+	
+	for(i=1;i<=num;i++)
+	{
+		fact=fact*i;
+		printf("%d\n",fact);
+	}
+	printf("factorial of %d number is %d \n",num,fact);
+	return 0;
+}
